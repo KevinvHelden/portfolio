c543d87c3fd95c80aa97b1e4071e05c143fa63d8
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-09-30 17:57:48 +0100

    fixed resizing and clickfunc on paragraph img

--- src/components/elements/Paragraph/Paragraph.tsx
+++ src/components/elements/Paragraph/Paragraph.tsx
@@ -3,32 +3,68 @@ import styles from "./Paragraph.module.scss";
 import classnames from "classnames";
 import { Text, Image } from "../";
 import maximize from "../../../images/icons/maximize.svg";
+import { getUrl } from '../../../firebase/helpers';
 
 type Props = {
   title?: string,
   text?: string,
   variant?: 'text' | 'image' | 'textAndImage' | 'textAndVideo',
-  image?: string,
-  imageAlt?: string | null,
-  imageClickFunc?: () => void,
+  image: string,
+  imageAlt: string,
+  imageClickFunc: (image: string, alt: string) => void,
   lifted?: boolean,
   reversed?: boolean,
   imageDescription?: string,
 }
 
-class Paragraph extends PureComponent<Props> {
+type State = {
+  image: string,
+}
+
+class Paragraph extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      image: "",
+    };
+  }
+
+  componentDidMount() {
+    const { image } = this.props;
+    this.getImage("paragraphImages", image);
+  }
+
+  componentDidUpdate() {
+    const { image } = this.props;
+    this.getImage("paragraphImages", image);
+  }
+
+  getImage = (folder: string, imageSource: string) => {
+    const { image } = this.props;
+    getUrl(folder, imageSource).then((src: string) => image !== src && this.setState({
+      image: src
+    }));
+  }
+
+  handleImageClick = () => {
+    const { imageAlt, imageClickFunc } = this.props;
+    const { image } = this.state;
+    imageClickFunc(image, imageAlt);
+  }
+
   render() {
     const {
       title = "",
       text = "",
       variant = 'text',
-      image = "",
       imageAlt = "",
-      imageClickFunc = () => { },
+      imageClickFunc,
       lifted,
       reversed,
       imageDescription,
     } = this.props;
+    const { image } = this.state;
+    const { handleImageClick } = this;
 
     return (
       <section className={classnames(styles.root, title, { [styles.lifted]: lifted })}>
@@ -43,7 +79,7 @@ class Paragraph extends PureComponent<Props> {
         {variant === "image" && imageAlt && (
           <div className={classnames(styles.imageContainer)}>
             <div className={classnames(styles.imageContainerInner)}>
-              <div onClick={imageClickFunc} className={classnames(styles.overlay)}>
+              <div onClick={handleImageClick} className={classnames(styles.overlay)}>
                 <div className={classnames(styles.overlay_inner)}>
                   <img src={maximize} alt={"maximize"} />
                   <Text text={"Enlarge"} strong />
@@ -70,7 +106,7 @@ class Paragraph extends PureComponent<Props> {
             </div>
             <div className={classnames(styles.textAndImageContainerImage)}>
               <div className={classnames(styles.imageContainerInner)}>
-                <div onClick={imageClickFunc} className={classnames(styles.overlay)}>
+                <div onClick={handleImageClick} className={classnames(styles.overlay)}>
                   <div className={classnames(styles.overlay_inner)}>
                     <img src={maximize} alt={"maximize"} />
                     <Text text={"Enlarge"} strong />
