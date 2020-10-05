903855b211563aaa4e149c5be01abd7183f43022
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-05 14:05:17 +0100

    added firebase sendemail function

--- src/firebase/functions/src/index.ts
+++ src/firebase/functions/src/index.ts
@@ -1,9 +1,40 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as nodemailer from "nodemailer";
+import config from "./mailConfig";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const RECEIVER_EMAIL = "k.v.helden96@hotmail.com";
+const SENDER_EMAIL = '"Kevin van Helden - message box" <k.v.helden96@hotmail.com>';
+
+function getParams(url: string) {
+  const _paramString: string = url.split("?")[1];
+  const _paramArray: Array<string> = _paramString.split("&");
+
+  let params: { [key: string]: string } = {};
+
+  _paramArray.forEach((param) => {
+    const [key, val] = param.split("=");
+    params[key] = decodeURI(val);
+  });
+
+  return params;
+}
+
+export const sendMail = functions
+  .region("europe-west2")
+  .https.onRequest((req, res) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    const { name, email, text } = getParams(req.url);
+
+    let transporter = nodemailer.createTransport(config);
+
+    const message = {
+      from: SENDER_EMAIL,
+      to: RECEIVER_EMAIL,
+      subject: `${name} -- ${email}`,
+      text,
+    };
+
+    transporter.sendMail(message);
+
+    res.send(true);
+  });
